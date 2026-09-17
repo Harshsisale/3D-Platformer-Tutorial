c7ba23d81b310d54a286d81046a4fe1f70196431
Harsh Sisale <sisaleha@msu.edu>
2026-09-17 03:34:17 -0400

    Started Teleport Script

--- Assets/Scripts/Environment/Teleport.cs
+++ Assets/Scripts/Environment/Teleport.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    
+    [Header("Settings")]
+    [Tooltip("The destination object with a Teleport script attached.")]
+    public Teleport destinationTeleporter;
+
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        
+            // reposition the player
+            Debug.Log(other.name + " collided with " + this.transform.parent.name + " so teleport to " + destinationTeleporter.transform.parent.name);
+    }
+
+    // OnTriggerExit is called when the Collider other has stopped touching the trigger
+    private void OnTriggerExit(Collider other)
+    {
+    }
 }
