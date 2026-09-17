d73e12e5f066e83b71b970bf7220812e4578b60f
Harsh Sisale <sisaleha@msu.edu>
2026-09-17 11:14:25 -0400

    Platform Script Updated

--- Assets/Scripts/Environment/MovingPlatform.cs
+++ Assets/Scripts/Environment/MovingPlatform.cs
@@ -9,7 +9,7 @@ public class MovingPlatform : MonoBehaviour
     public Transform pointB;
 
     [Header("Movement Settings")]
-    public float speedMultiplier = 0.5f;
+    public float speed = 0.5f;
     public float waitTime = 1;
 
     private bool waiting = false;
@@ -20,11 +20,50 @@ public class MovingPlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-   
+      transform.position = pointA.position;
     }
+
     // Update is called once per frame
     void Update()
     {
-   
+      if (waiting==true)
+      {
+        Wait();
+      } else
+      {
+        Move();
+      }
+    }
+
+    private void Wait()
+    {
+      timeWaiting += Time.deltaTime;
+      if (timeWaiting >= waitTime)
+      {
+        timeWaiting = 0;
+        waiting = false;
+      }
+    }
+
+    private void Move()
+    {
+      if (movingTowardB)
+      {
+        percentMoved += Time.deltaTime * speed;
+        if (percentMoved >= 1)
+        {
+          waiting = true;
+          movingTowardB = false;
+        }
+      } else
+      {
+        percentMoved -= Time.deltaTime * speed;
+        if (percentMoved <= 0)
+        {
+        waiting = true;
+        movingTowardB = true;
+        }
+      }
+      transform.position = Vector3.Lerp(pointA.position, pointB.position, percentMoved);
     }
 }
