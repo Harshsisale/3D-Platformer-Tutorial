592647a9143880c29f50be5c229e11d4b33a2684
Harsh Sisale <sisaleha@msu.edu>
2026-09-17 04:36:11 -0400

    Level1 able to be played
    Camera bugs out on y axis after teleporting need to investigate

--- Assets/Scripts/Environment/Teleport.cs
+++ Assets/Scripts/Environment/Teleport.cs
@@ -8,19 +8,46 @@ public class Teleport : MonoBehaviour
     [Tooltip("The destination object with a Teleport script attached.")]
     public Teleport destinationTeleporter;
 
+    [Tooltip("The effect to create when teleporting.")]
+    public GameObject teleportEffect;
+
     // teleporterAvailable tracks the state of the teleporter. It is used to turn off the destination Teleporter just before the player is moved to it so the player does not teleport back immediately.
     private bool teleporterAvailable = true;
 
     // OnTriggerEnter is called when the Collider other enters the trigger
     private void OnTriggerEnter(Collider other)
     {
-      if ((other.tag == "Player") && (teleporterAvailable==true))
+      if ((other.tag == "Player") && (teleporterAvailable==true) && (destinationTeleporter != null))
       {
             Debug.Log(other.name + " collided with " + this.transform.parent.name + " so teleport to " + destinationTeleporter.transform.parent.name);
+
+            // spawn the teleport effect
+            if (teleportEffect != null)
+            {
+              Instantiate(teleportEffect, transform.position, transform.rotation, null);
+            }
+
             // turn off destination teleporter so it will not teleport player right back
             destinationTeleporter.teleporterAvailable = false;
+
+            // turn off Character Controller if there is one, so we can move this gameObject
+            CharacterController characterController = other.gameObject.GetComponent<CharacterController>();
+            if (characterController != null)
+            {
+              characterController.enabled = false;
+            }
+
+            // calculate height offset of the player from the Teleporter middle
+            float heightOffset = transform.position.y - other.transform.position.y;
+
             // reposition the player
-            other.transform.position = destinationTeleporter.transform.position
+            other.transform.position = destinationTeleporter.transform.position - new Vector3(0,heightOffset,0);
+
+            // if character controller is specified then turn it back on
+            if (characterController != null)
+            {
+              characterController.enabled = true;
+            }
       }
     }
 
